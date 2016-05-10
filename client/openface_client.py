e660bf2226df5417ea2e3e2210b8ed8234ef62b2
Ben Johnson <ben@gophronesis.com>
2016-05-10 10:19:10 -0400

    adding face extraction code

--- client/openface_client.py
+++ client/openface_client.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import re
 import json
 import cv2
 
@@ -14,20 +15,32 @@ class OpenFaceClient:
         bgrImg = cv2.imread(imgPath)
         rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
         fltImg = rgbImg.flatten().tolist()
-        return fltImg, rgbImg.shape
+        return fltImg, bgrImg, rgbImg.shape
 
 
     def check_status(self):
         cmd = self.url + "/api/health"
-        r = requests.post(cmd)
+        r = requests.get(cmd)
         r.raise_for_status()
         return r.json()
 
 
     def get_face_bb(self, imgPath):
         cmd = self.url + "/api/bbox"
-        fltImg, dim = self.read_image(imgPath)
+        fltImg, bgrImg, dim = self.read_image(imgPath)
         payload = {'image': fltImg, 'dim': dim}
         r = requests.post(cmd, json=payload, headers= {})
         r.raise_for_status()
-        return r.json()
+        return r.json(), bgrImg
+    
+    def faces_to_file(self, imgPath, margin=10):
+        boxes, bgrImg = self.get_face_bb(imgPath)
+        for i, box in enumerate(boxes['rects']):
+            print 'extracting face %i at %s' % (i, str(box))
+            outpath = '%s-face-%d.png' % (re.sub('\.*?$', '', imgPath), i)
+            
+            (top, bottom, left, right) = box
+            im_sub = bgrImg[max(0, top - margin):(bottom + margin), max(0, left - margin):(right + margin), :]
+            cv2.imwrite(outpath, im_sub)
+        
+        return True
